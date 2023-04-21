98415ddc7f66e174b4e3fab665fc1b47add25d20
AugustoTI <augustocesar4001@gmail.com>
2023-04-21 17:45:54 -0300

    refactor: :boom: change initial files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/global.css'
+import { type NextPage } from 'next'
+import { type AppProps } from 'next/app'
+import { type ReactElement, type ReactNode } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
 export default MyApp
