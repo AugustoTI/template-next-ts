fa285dd0e8f0dd9edfc31ea27cbf69739717dff3
Augusto César <augustocesarti2021@gmail.com>
2023-06-12 15:30:35 +0000

    feat: metadata example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { type Metadata } from 'next'
 import { type FC } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Create Next App',
+  description: 'This site was created using the NextJS framework 🚀',
+}
 
 const HomePage: FC = () => {
   return <h1>Hello, Next.js!</h1>
