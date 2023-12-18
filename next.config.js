376e2050a76d36c298e64d03861ae45a08e26153
AugustoTI <augustocesar4001@gmail.com>
2023-12-18 14:31:41 -0300

    feat: implement PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 const isDev = process.env.NODE_ENV !== 'production'
 
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  disable: isDev,
+  reloadOnOnline: true,
+})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   compiler: {
     removeConsole: !isDev,
   },
-}
+})
 
 module.exports = nextConfig
