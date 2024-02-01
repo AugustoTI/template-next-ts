41af3d63412be12ba50c6ab40b89e47c888e58ac
AugustoTI <augustocesar4001@gmail.com>
2024-01-31 21:42:54 -0300

    build: update packages

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
+import('./src/env.mjs')
+
 const isDev = process.env.NODE_ENV !== 'production'
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  disable: isDev,
-  reloadOnOnline: true,
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   compiler: {
     removeConsole: !isDev,
   },
-})
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+}
 
 module.exports = nextConfig
