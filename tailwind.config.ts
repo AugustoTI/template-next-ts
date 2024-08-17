f1dacdc92ced25141c8c8714fd3f67d2a986102a
AugustoTI <augustocesar4001@gmail.com>
2024-08-17 16:55:17 -0300

    chore: update packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from 'tailwindcss'
+
+export default {
   content: ['./src/**/*.{ts,tsx,mdx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config
