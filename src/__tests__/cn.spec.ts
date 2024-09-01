671c58862e5d8fa410f4728297aaf099da24ba97
AugustoTI <augustocesar4001@gmail.com>
2024-09-01 14:30:53 -0300

    chore: replaces the commitlint package for a faster, more user-friendly message linter

--- src/__tests__/cn.spec.ts
+++
@@ -1,9 +0,0 @@
-import { cn } from '@/libs/utils'
-
-describe('utils/cn', () => {
-  it('should overwrite the previous className', () => {
-    const className = cn('px-2', 'px-4')
-
-    expect(className).toBe('px-4')
-  })
-})
