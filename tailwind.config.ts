d530e7afcf4a60bd6b6308f18ca17bfa043ec240
AugustoTI <augustocesar4001@gmail.com>
2024-10-17 19:13:19 -0300

    chore: update packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
+import pluginScrollbar from 'tailwind-scrollbar'
 import { type Config } from 'tailwindcss'
+import pluginAnimations from 'tailwindcss-animate'
 
 export default {
   content: ['./src/**/*.{ts,tsx,mdx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [pluginAnimations, pluginScrollbar],
 } satisfies Config
