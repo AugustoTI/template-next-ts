b01dc2be9e6e5940cbaf41a3011fc4313b149050
Augusto César <89668244+AugustoTI@users.noreply.github.com>
2025-03-09 02:16:32 -0300

    chore: change regex intellisense for tailwind

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import { cx } from 'class-variance-authority'
-import { type ClassValue } from 'class-variance-authority/types'
+import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(cx(inputs))
+  return twMerge(clsx(inputs))
 }
