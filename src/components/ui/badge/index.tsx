3837e86163b4747a0c3eb5aaba719d16060ebcc7
AugustoTI <augustocesar4001@gmail.com>
2026-04-19 20:32:09 -0300

    refactor: remove old components

--- src/components/ui/badge/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ark, type HTMLProps } from '@ark-ui/react/factory'
-
-import { badgeVariants, type BadgeVariantsProps } from './badge.styles'
-
-interface BadgeProps extends HTMLProps<'span'>, BadgeVariantsProps {}
-
-export function Badge({ className, variant = 'primary', ...props }: BadgeProps) {
-  return <ark.span className={badgeVariants({ className, variant })} {...props} />
-}
