615f053d7dd3cbbd0c0243039573a63ccc08e649
AugustoTI <augustocesar4001@gmail.com>
2026-04-30 12:07:33 -0300

    docs: add tooltip provider on storybook

--- src/components/ui/tooltip/tooltip.stories.tsx
+++ src/components/ui/tooltip/tooltip.stories.tsx
@@ -2,6 +2,7 @@ import { type Meta, type StoryObj } from '@storybook/nextjs'
 
 import { Tooltip } from '.'
 import { Button } from '../button'
+import { Icons } from '../icons'
 
 export default {
   title: 'UI/Tooltip',
@@ -13,16 +14,33 @@ type Story = StoryObj
 export const Default: Story = {
   render() {
     return (
-      <Tooltip>
-        <Tooltip.Trigger render={Button}>Hover me</Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Positioner>
-            <Tooltip.Content>
-              <p>Tooltip content</p>
-            </Tooltip.Content>
-          </Tooltip.Positioner>
-        </Tooltip.Portal>
-      </Tooltip>
+      <Tooltip.Provider>
+        <div className="flex items-center gap-2">
+          <Tooltip>
+            <Tooltip.Trigger render={Button}>Hover me</Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Positioner>
+                <Tooltip.Content>
+                  <p>Tooltip content</p>
+                </Tooltip.Content>
+              </Tooltip.Positioner>
+            </Tooltip.Portal>
+          </Tooltip>
+
+          <Tooltip>
+            <Tooltip.Trigger render={<Button size="icon-lg" variant="secondary" />}>
+              <Icons.Settings />
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Positioner>
+                <Tooltip.Content>
+                  <p>Settings</p>
+                </Tooltip.Content>
+              </Tooltip.Positioner>
+            </Tooltip.Portal>
+          </Tooltip>
+        </div>
+      </Tooltip.Provider>
     )
   },
 }
