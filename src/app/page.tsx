42ac24879024b8230dffd2bf50309ad50191d9cc
AugustoTI <augustocesar4001@gmail.com>
2026-07-18 01:41:52 -0300

    feat: add internalization support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function HomePage() {
+import { getT } from 'next-i18next/server'
+
+export default async function HomePage() {
+  const { t } = await getT('home')
+
   return (
     <>
-      <h1 className="text-6xl font-bold">Bem vindo</h1>
+      <h1 className="text-6xl font-bold">{t('title')}</h1>
     </>
   )
 }
